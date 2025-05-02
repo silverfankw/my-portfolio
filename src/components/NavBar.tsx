9940334526363b626f3a8784e8b334249a61cccb
Silver Fan <SilverF@tl.com>
2025-05-03 04:24:04 +0800

    fix intersection observer threshold

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -39,7 +39,7 @@ const NavBar: React.FC = () => {
         const options = {
             root: null,
             rootMargin: "0px",
-            threshold: [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
+            threshold: [0.15],
         };
 
         const observer = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
